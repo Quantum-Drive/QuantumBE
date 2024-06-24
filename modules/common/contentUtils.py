ac43102ec6a5516367e692a8a7391d974ff1915f
JuhyunKim <juhy0987@naver.com>
2024-06-24 14:03:16 +0900

    feat: Rename imgUtils.py to contentUtils.py and add video image extraction function

--- modules/common/contentUtils.py
+++ modules/common/contentUtils.py
@@ -1,6 +1,9 @@
-from PIL import Image
 import base64
 import io
+
+from PIL import Image
+from moviepy.editor import VideoFileClip
+
 
 def createThumbnail(imagePath, thumbnailSize=(128, 128), quality=85, format='JPEG'):
   with Image.open(imagePath) as img:
@@ -17,3 +20,11 @@ def createThumbnail(imagePath, thumbnailSize=(128, 128), quality=85, format='JPE
     imgStr = base64.b64encode(buffered.getvalue()).decode()
     dataURL = f"data:image/{format.lower()};base64,{imgStr}"
     return dataURL
+
+def createVideoImg(videoPath, time=1.0):
+  with VideoFileClip(videoPath) as clip:
+    frame = clip.get_frame(time)
+    img = Image.fromarray(frame)
+    
+    return img
+
