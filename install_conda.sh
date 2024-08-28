00d4910800ca1956cff4261b6e2ceadc6ff15605
JuhyunKim <juhy0987@naver.com>
2024-08-28 17:15:43 +0900

    Update project dependencies in Pipfile and requirements.txt

--- install_conda.sh
+++
@@ -1,6 +0,0 @@
-# install_conda.sh
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda init bash
-source ~/.bashrc
