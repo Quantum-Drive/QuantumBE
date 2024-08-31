24303d6c51db0a28ae92d6c9e6d9e22372c495d3
JuhyunKim <juhy0987@naver.com>
2024-08-31 12:54:56 +0900

    refactor: Update fileUtils.py to remove unused code and improve functions
    The code changes in fileUtils.py remove unused code related to image processing, video editing, and PDF conversion. Additionally, the getThumbnail function is improved by using tempfile for temporary file handling. These changes enhance the overall efficiency and maintainability of the fileUtils module.

--- modules/common/fileUtils.py
+++ modules/common/fileUtils.py
@@ -6,12 +6,8 @@ import hashlib
 import shutil
 import base64
 import requests
-import imageio
-import numpy as np
 
-import fitz
 from PIL import Image
-from moviepy.editor import VideoFileClip
 
 def pathSplit(sPath="/"):
   if not sPath:
@@ -103,42 +99,3 @@ def img2DataURL(img: Image.Image, format: str = "PNG"):
   img.save(imgByteArr, format=format)
   imgByteArr = imgByteArr.getvalue()
   return f"data:image/{format.lower()};base64,{base64.b64encode(imgByteArr).decode('utf-8')}"
-
-
-def thumbnail(img: Image.Image, size=(128, 128), quality=85) -> Image.Image:
-  img.thumbnail(size)
-  imgIO = io.BytesIO()
-  img.save(imgIO, format="png", quality=quality)
-  imgIO.seek(0)
-  
-  return imgIO
-
-def clipVideo(videoBytes, format="mp4", time: float = 1.0):
-  videoIO = io.BytesIO(videoBytes)
-  reader = imageio.get_reader(videoIO, format=format)
-  frameNum = time * 3600
-  
-  numFrames = reader.get_length()
-  print(reader.get_length())
-  if numFrames < frameNum:
-    frameNum = 10
-    
-  frame = reader.get_data(frameNum)
-  return Image.fromarray(frame)
-  
-def pdf2Image(pdfPath: str, offset=0, limit=1e9):
-  lImgs = []
-  
-  pdfDocument = fitz.open(pdfPath)
-  pdfLast = min(offset+limit, pdfDocument.page_count)
-  
-  for i in range(offset, pdfLast):
-    page = pdfDocument.load_page(i)
-    
-    pixmap = page.get_pixmap()
-    bImage = pixmap.tobytes()
-    img = Image.frombytes(pixmap.colorspace, pixmap.size, bImage)
-
-    lImgs.append(img)
-  return lImgs, -1 if i >= pdfDocument.page_count-1 else i+1
-    
